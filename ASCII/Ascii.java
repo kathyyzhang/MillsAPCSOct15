60e4f1cf62ca0c7a77cbdcc3c548fa046ad6af52
kathyyzhang <kathyzhang168@gmail.com>
2015-10-01 08:53:23 -0700

    ok

---
+++ ASCII/Ascii.java
@@ -0,0 +1,53 @@
+/**
+ * prints a cool looking hibiscus flower
+ * 
+ * @author Kathy Zhang 
+ * 10.1.15
+ */
+public class Ascii
+{
+   public static void printFlower()
+   {
+System.out.println("                             ___ .");
+//System.out.println("                   _~/"|  ~~ ~.\.~");
+System.out.println("                  /   :   /   /  \\_");
+System.out.println("                 ?\"\"  ;  `  ,\'     )");
+System.out.println("        /-,      /    .   .     .  |");
+System.out.println("       /   ~-_  /   dp   .    .    |");
+System.out.println("    .%\"\":    .? |:    . .   .      |");
+System.out.println("   %    \'.   . \\|:. . ..  .   .    /");
+System.out.println(" //  \'.   \'.: : :| :   .. . .    _/");
+System.out.println("// ~~~::.   .:: ::. . .::.:. ..\'~");
+System.out.println("\\::... `\'.. : : : \" . :. .  ~~|    _____");
+System.out.println("(:::. ..   \'.: .:.::. : . : \"/__-\"\"     --");
+System.out.println(" )\"\".>  --~~~__. ::::*## :.:.:)  :    .:` ~\\");
+System.out.println("|..::\'      >>    #######  \'.: .\' .  .`     ?");
+System.out.println("\\_.___      :\'\'> ######### ::.\".    ~ ..~\'\" |,");
+System.out.println("     _\">~____~~::. #* )#### :  ..~       .   )");
+System.out.println("    /:.:\'\';.:::_-  _-#####*:;: ::>,,          }");
+/*System.out.println("   o__o___~~_-\" _-\"###### .:.\'\'     ..~~ ~    |");
+//System.out.println("  ( o() &@&__--/"  '::####::/"  /";..    ~~...::.)");
+System.out.println(" (o& O& O?S).`   .:\':::*: :      ~~~...     \"\");
+System.out.println("   oS %@&>/    .:\'  ::::: :.       .~  \"\'.  {");
+System.out.println("   \~*~~\"\"   __:/   "::::..L        ~     \'\'/");
+System.out.println("    |::\'    /##)     :\':::::\         .   %");
+System.out.println("    \\:; .  |#/       : :::::::-___      -/"");
+System.out.println("     \\::::: |        : :::::::::::\----/"");
+System.out.println("     \\::::::|:.      : :::::::::::|");
+System.out.println("     \\::::::|\'     . : ::::::::::::|");
+System.out.println("      -::::/ \\     :.\'::::::::::::/");
+System.out.println("        ~__/  \\.   ;:.::::::::::_/   ");  
+System.out.println("               \\::.::::::::::::/");
+System.out.println("                 --_;::::::_-~/");
+System.out.println("                     \"\"\"\"\"\"");*/
+}   
+    
+    public static void printEntire()
+    {
+        //parameters: height, width
+    }
+    public static void main(String[] args)
+    {
+        
+    }
+}
