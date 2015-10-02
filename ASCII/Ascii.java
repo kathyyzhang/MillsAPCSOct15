6d01c75073b5e842b3d195236f2978fb5c05f458
kathyyzhang <kathyzhang168@gmail.com>
2015-10-02 08:54:22 -0700

    hi

--- ASCII/Ascii.java
+++ ASCII/Ascii.java
@@ -1,156 +1,159 @@
 /**
- * prints a cool looking hibiscus flower
+ * prints a cool looking hibiscus flower according to input of height and width
  * 
- * @author Kathy Zhang 
- * 10.1.15
+ * @author Kathy Zhang :)
+ * 10.2.15
  */
 public class Ascii
 {
     public static void main(String[] args)
     {
-        printFlower(3,3);
+        //(height, width) of print
+        printFlower(2,3);
 
     }
     public static void printFlower(int height, int width)
     {
         for (int j=1; j<=height; j++)
+        //controls amount of rows there are
         {
+            //i<=width controls amount of columns
             for (int i=1; i<=width; i++)
             {
                 System.out.print("                               _~-==  ~~  ,-.                               ");
             }
             System.out.println();
-            for (int i=1; i<=height; i++)
+            for (int i=1; i<=width; i++)
             {
                 System.out.print("                              /   :   / . /  \\_                             ");
             }
             System.out.println();
-            for (int h=1; h<=height; h++)
+            for (int h=1; h<=width; h++)
             {
                 System.out.print("                             /    .   .     .   |                           ");
             }
             System.out.println();
-            for (int i=1; i<=height; i++)
+            for (int i=1; i<=width; i++)
             {
                 System.out.print("                   /---~-_  /   kz   .    .     |                           ");
             }
             System.out.println();
-            for (int l=1; l<=height; l++)
+            for (int l=1; l<=width; l++)
             {
                 System.out.print("                .%\"\":    .? |:    . .   .   .   |                           ");
             }
             System.out.println();
-            for (int m=1; m<=height; m++)
+            for (int m=1; m<=width; m++)
             {
                 System.out.print("               %    \'.   . \\|:. . ..  .   .   . /                           ");
             }
             System.out.println();
-            for (int n=1; n<=height; n++)
+            for (int n=1; n<=width; n++)
             {
                 System.out.print("             //  \'.   \'.: : :| :   .. . .    . _/                           ");
             }
             System.out.println();
-            for (int o=1; o<=height; o++)
+            for (int o=1; o<=width; o++)
             {
                 System.out.print("            // ~~~::.   .:: ::. . .::.:. .  .  .\'                           ");
             }
             System.out.println();
-            for (int p=1; p<=height; p++)
+            for (int p=1; p<=width; p++)
             {
                 System.out.print("            \\::... `\'.. : : : \" . :. .       ~~/  ____                      ");
             }
             System.out.println();
-            for (int i=1; i<=height; i++)
+            for (int i=1; i<=width; i++)
             {
                 System.out.print("            (:::. ..   \'.: .:.::. : . : \" \" /  -\"\"     --                   ");
             }
             System.out.println();
-            for (int r=1; r<=height; r++)
+            for (int r=1; r<=width; r++)
             {
                 System.out.print("             )\"\".>  --~~~__. ::::*##### :.:.:)  :    .:` ~\\                 ");
             }
             System.out.println();
-            for (int s=1; s<=height; s++)
+            for (int s=1; s<=width; s++)
             {
                 System.out.print("            |..::\'      >>    ############  \'.: .\' .  .`   |                ");
             }
             System.out.println();
-            for (int t=1; t<=height; t++)
+            for (int t=1; t<=width; t++)
             {
                 System.out.print("            \\_.___      :\'\'> ############ ::.\".    ~ ..~\'\"                  ");
             }
             System.out.println();
-            for (int u=1; u<=height; u++)
+            for (int u=1; u<=width; u++)
             {
                 System.out.print("                 _\">~____~~::. #####* )#### :  ..~       .   )              ");
             }
             System.out.println();
-            for (int g=1; g<=height; g++)
+            for (int g=1; g<=width; g++)
             {
                 System.out.print("                /:.:\'\';.:::_:#- -= _-#####*:;: ::>,,          }             ");
             }
             System.out.println();
-            for (int v=1; v<=height; v++)
+            for (int v=1; v<=width; v++)
             {
                 System.out.print("               o__o___~~_-\" _--/######### .:.\'\'     ..~~ ~ __/              ");
             }
             System.out.println();
-            for (int w=1; w<=height; w++)
+            for (int w=1; w<=width; w++)
             {
                 System.out.print("              ( o() &@&__--\"  \'::####::\"  \";..    ~~...::.)                 ");
             }
             System.out.println();
-            for (int x=1; x<=height; x++)
+            for (int x=1; x<=width; x++)
             {
                 System.out.print("             (o& O& O?S).`   .:\':::*: :   .  ~~~...     \"                   ");
             }
             System.out.println();
-            for (int y=1; y<=height; y++)
+            for (int y=1; y<=width; y++)
             {
                 System.out.print("               oS %@&>/    .:\'  ::::: :.   .   .~  \"\'.  {                   ");
             }
             System.out.println();
-            for (int i=1; i<=height; i++)
+            for (int i=1; i<=width; i++)
             {
                 System.out.print("               \\~*~~\"\"   __:/   \"::::..L .      ~     \'\'/                   ");
             }
             System.out.println();
-            for (int a=1; a<=height; a++)
+            for (int a=1; a<=width; a++)
             {
                 System.out.print("                |::\'    /##)     :\':::::\\     .   .   %                     ");
             }
             System.out.println();
-            for (int xx=1; xx<=height; xx++)
+            for (int xx=1; xx<=width; xx++)
             {
                 System.out.print("                \\:; .  |#/       : :::::::-___.     -\"                      ");
             }
             System.out.println();
-            for (int b=1; b<=height; b++)
+            for (int b=1; b<=width; b++)
             {
                 System.out.print("                 \\::::: |        : :::::::::::\\----\"                        ");
             }
             System.out.println();
-            for (int c=1; c<=height; c++)
+            for (int c=1; c<=width; c++)
             {
                 System.out.print("                 \\::::::|:.      : :::::::::::|                             ");
             }
             System.out.println();
-            for (int d=1; d<=height; d++)
+            for (int d=1; d<=width; d++)
             {
                 System.out.print("                 \\::::::|\'     . : ::::::::::::|                            ");
             }
             System.out.println();
-            for (int e=1; e<=height; e++)
+            for (int e=1; e<=width; e++)
             {
                 System.out.print("                    ~__/   \\   :::.::::::::::::/                            ");  
             }
             System.out.println();
-            for (int f=1; f<=height; f++)
+            for (int f=1; f<=width; f++)
             {
                 System.out.print("                               _;::::::_-~/                                 ");
             }
             System.out.println();
-            for (int i=1; i<=height; i++)
+            for (int i=1; i<=width; i++)
             {
                 System.out.print("                                 \"\"\"\"\"\"                                     ");
             }   
