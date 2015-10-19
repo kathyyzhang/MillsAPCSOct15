405f7a4b09600822982237c75bffff6c2b01f717
kathyyzhang <kathyzhang168@gmail.com>
2015-10-18 22:16:29 -0700

    k

--- Equestria/Equestria.java
+++ Equestria/Equestria.java
@@ -1,14 +1,14 @@
 /**
- * Write a description of class Equestria here.
+ * Finds distances between places in Equestria. Also serves as a handy tour guide.
  * 
  * @author Kathy Zhang
- * @version 10.13.15
+ * @version 10.17.15
  */
 public class Equestria
 {
     public static void main(String[] args)
     {
-        /*//Baltimare
+        //Baltimare
         int xbalt=31;
         int ybalt=14;
         //Manehattan
@@ -25,13 +25,40 @@ public class Equestria
         int ybad=22;
         //Ponyville
         int xpony=17;
-        int ypony=13;*/
-        double d=distance(8, 17, 27, 22);
-
+        int ypony=13;
+        //Cloudville
+        int xcloud=15;
+        int ycloud=8;
+        //Dodge City
+        int xdodge =23;
+        int ydodge=17;
+        //Canterlot
+        int xcant=18;
+        int ycant=9;
+        //double d=distance(8, 17, 27, 22);
+        double d=distance(xbalt, ybalt, xmane, ymane);
         System.out.println("Distance from Baltimare to Manehattan = " + d);
-        dist(5, 6, 7, 2, 3, 4);
-        //dist(8, 17, 27, 22, 17, 13);
-        //roadTrip();
+        double d2=distance(xlos, ylos, xneigh, yneigh);
+        System.out.println("Distance from Los Pegasus to Neighagara Falls = " + d2);
+        double d3=distance(xbad, ybad, xpony, ypony);
+        System.out.println("Distance from Badlands to Ponyville = " + d3);
+        //diameter= distance from Cloudville to Ponyville
+        double dia=distance(xcloud, ycloud, xpony, ypony);
+        System.out.println("The diameter of the trip is "+dia +".");
+        double trip=roadTrip(dia);
+        System.out.println("The total length of the trip is "+trip);
+        //three points: Manehattan, Dodge City, Canterlot 
+        double x=distance(xmane, ymane, xdodge, ydodge);
+        double y=distance(xdodge, ydodge, xcant, ycant);
+        double z=distance(xcant, ycant, xmane, ymane);
+        System.out.println("Distance from Manehattan to Dodge City = " + x);
+        System.out.println("Distance from Dodge City to Canterlot = " + y);
+        System.out.println("Distance from Canterlot to Dodge City = " + z);
+        tour(x, y, z);
+        //four-coordinate trip consists of Ponyville, Manehattan, Badlands, and Cloudville
+        double max= longestTrip(xpony, ypony, xmane, ymane, xbad, ybad, xcloud, ycloud);
+        System.out.println("The longest distance is between all pairs of destinations is "+max+ "."); 
+        
     }
 
     public static double distance(int x1, int y1, int x2, int y2){
@@ -41,16 +68,40 @@ public class Equestria
 
     }
 
-    /*public static void roadTrip(){
-    int diameter=35;
-    double c=diameter*Math.PI;
-    return c;*/
+    public static double roadTrip(double diameter){
+        //diameter= distance from Cloudville to Ponyville
+        double c=diameter*Math.PI*100;
+        int c2=(int)c;
+        return 0.01*c2;
+    }
 
-    public static void dist(int x1, int y1, int x2, int y2, int x3, int y3){ 
+    public static void tour(double d1, double d2, double d3){
+        //d1=Manehattan, d2=Dodge City, d3=Canterlot
+        //angle between d2 and d3
+        double a= Math.acos((Math.pow(d1,2)-Math.pow(d2,2)-Math.pow(d3,2))/(-2*d2*d3))*(180/Math.PI)*100;
+        int a2=(int)a;
+        
+        //angle between d1 and d3
+        double b= Math.acos((Math.pow(d2,2)-Math.pow(d1,2)-Math.pow(d3,2))/(-2*d1*d3))*(180/Math.PI)*100;
+        int b2=(int)b;
+        //angle between d2 and d1
+        double c= Math.acos((Math.pow(d3,2)-Math.pow(d2,2)-Math.pow(d1,2))/(-2*d1*d2))*(180/Math.PI)*100;
+        int c2=(int)c;
+        System.out.println("The angle between Dodge City and Canterlot is "+0.01*a2+" degrees.");
+        System.out.println("The angle between Manehattan and Canterlot is "+0.01*b2+" degrees.");
+        System.out.println("The angle between Dodge City and Manehattan is "+0.01*c2+" degrees.");
+
+    }
+
+    public static double longestTrip(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4){ 
         double distx1x2= distance(x1, y1, x2, y2);
-        double distx2x3= distance(x2, y2, x3, y3);
         double distx1x3= distance(x1, y1, x3, y3);
-        System.out.println(distx1x2);
+        double distx1x4= distance(x1, y1, x4, y4);
+        double distx2x3= distance(x2, y2, x3, y3);
+        double distx2x4= distance(x2, y2, x4, y4);
+        double distx3x4= distance(x3, y3, x4, y4);
+        return Math.max(Math.max(Math.max(distx1x2, distx1x3), Math.max(distx1x4, distx2x3)), Math.max(distx2x4, distx3x4));
+        
     }  
 
 }
